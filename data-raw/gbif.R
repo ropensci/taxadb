15fe167ffcaa79eb3a4b4e27ee71eda21191591f
Carl Boettiger <cboettig@gmail.com>
2018-06-06 14:54:20 +0000

    gbif in wide & long

--- data-raw/gbif.R
+++ data-raw/gbif.R
@@ -2,15 +2,38 @@
 library(taxizedb) 
 library(tidyverse)
 
-
-
 gbif <- db_download_gbif()
 db_load_gbif()## not needed
 gbif_db <- src_gbif(gbif)
 
+gbif_taxa <- tbl(gbif_db, "gbif") %>% 
+  select(taxon_id = taxonID, scientificName, rank= taxonRank, 
+         kingdom, phylum, class = clazz, order = ordder, family, genus,
+         canonicalName, genericName, taxonomicStatus,
+         specificEpithet, infraspecificEpithet) %>% 
+  collect()
 
-gbif_taxa <- tbl(gbif_db, "gbif")  %>%
-  collect() ## Only table
+gbif_taxa <- 
+gbif_taxa %>% 
+  left_join(gbif_taxa %>% 
+              filter(rank == "species") %>% 
+              select(taxon_id, species = scientificName))
+
+
+gbif_wide <- gbif_taxa %>% 
+  select(taxon_id, 
+         kingdom, phylum, class, order, family, genus, species,
+         specific_epithet = specificEpithet, infraspecific_epithet = infraspecificEpithet,
+         genericName, taxonomicStatus)
+write_tsv(gbif_wide, "data/gbif_wide.tsv.bz2")
+rm(gbif_wide)
+
+gbif_long <- 
+  gbif_taxa %>% 
+  select(taxon_id, name = scientificName, rank = rank, genericName, taxonomicStatus)
+
+write_tsv(gbif_long, "data/gbif_long.tsv.bz2")
+
 write_tsv(gbif_taxa, "data/gbif.tsv.bz2")
 rm(gbif_taxa)
 
