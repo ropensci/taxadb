8680f72e9d74343d723edca483a0dd142648be2d
Carl Boettiger <cboettig@gmail.com>
2018-06-10 23:45:24 +0000

    fix col names in COL

--- data-raw/col.R
+++ data-raw/col.R
@@ -17,24 +17,34 @@ col_taxa <- tbl(col_db, "_species_details")
 #col_taxa <- col_taxa[!drop]
 
 col_taxa <- col_taxa %>% 
-  select(taxon_id, kingdom_name, phylum_name, class_name, order_name,  superfamily_name, family_name, genus_name, subgenus_name, species_name, infraspecies_name,
-       kingdom_id, phylum_id, class_id, order_id,  superfamily_id, family_id, genus_id, subgenus_id,  species_id,  infraspecies_id,
-       is_extinct) %>% collect()
+  select(taxon_id, kingdom_name, phylum_name, class_name, order_name,
+         superfamily_name, family_name, genus_name, subgenus_name,
+         species_name, infraspecies_name,  kingdom_id, phylum_id,
+         class_id, order_id,  superfamily_id, family_id, genus_id,
+         subgenus_id,  species_id,  infraspecies_id, is_extinct) %>% collect()
 
 
 ## Transform to long form
-col_names <- col_taxa %>% select(taxon_id, kingdom = kingdom_name, phylum = phylum_name, class = class_name, 
-                                 order = order_name,  superfamily = superfamily_name, family = family_name, 
-                                 genus = genus_name, subgenus = subgenus_name, species = species_name, infraspecies = infraspecies_name)
-col_ids <- col_taxa %>% select(taxon_id, kingdom = kingdom_id, phylum = phylum_id, class = class_id, 
-                               order = order_id, superfamily = superfamily_id, family = family_id, genus = genus_id, 
-                               subgenus = subgenus_id,  species = species_id,  infraspecies = infraspecies_id)
-other <- col_taxa %>%  select(taxon_id, is_extinct)
+col_names <- col_taxa %>% 
+  select(taxon_id, kingdom = kingdom_name, phylum = phylum_name, class = class_name, 
+         order = order_name,  superfamily = superfamily_name, family = family_name, 
+         genus = genus_name, subgenus = subgenus_name, 
+         species = species_name, infraspecies = infraspecies_name)
+col_ids <- col_taxa %>% 
+  select(taxon_id, kingdom = kingdom_id, phylum = phylum_id, class = class_id, 
+         order = order_id, superfamily = superfamily_id, family = family_id,
+         genus = genus_id, subgenus = subgenus_id,  
+         species = species_id,  infraspecies = infraspecies_id)
 
-other %>% mutate(is_extinct <- as.logical(is_extinct))
+other <- col_taxa %>%  select(taxon_id, is_extinct) %>%
+  mutate(is_extinct <- as.logical(is_extinct))
 
-sci_names <- col_names %>% select(taxon_id, genus, species) %>% tidyr::unite(name, genus, species, sep = " ")
-long_names <- col_names %>% gather(rank, path, -taxon_id) %>% left_join(sci_names) %>% select(taxon_id, name, path, rank)
+sci_names <- col_names %>% select(taxon_id, genus, species) %>% 
+  tidyr::unite(name, genus, species, sep = " ")
+
+long_names <- col_names %>% gather(rank, path, -taxon_id) %>%
+  left_join(sci_names) %>% select(taxon_id, name, path, rank)
+
 long_ids <- col_ids %>% gather(rank, path_id, -taxon_id)
 
 
