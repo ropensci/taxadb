b4b72586bc94c91d606e455ef391a1e695f7a316
Carl Boettiger <cboettig@gmail.com>
2018-06-10 21:48:43 +0000

    create_db

--- data-raw/create_db.R
+++ data-raw/create_db.R
@@ -2,8 +2,22 @@
 
 library(arkdb)
 
-files <- fs::dir_ls("data/", glob="*.tsv.bz2")
-db <- unark(files, dbname = "data/taxa.sqlite", lines = 1e6)
+## FIXME currently only *_wide tables have id as unique to row.  
+files <- fs::dir_ls("data/", glob="*wide.tsv.bz2")
+db <- unark(files, dbname = "taxa.sqlite", lines = 1e6)
 
-R.utils::bzip2("data/taxa.sqlite")
+## Set id as primary key in each table.  
+
+tbls <- DBI::dbListTables(db$con)
+
+lapply(tbls, function(table)
+glue::glue("ALTER TABLE {table} ADD PRIMARY KEY ({key});", 
+           table = table, key = "id"))
+
+DBI::dbDisconnect(db$con)
+
+R.utils::bzip2("taxa.sqlite", remove = FALSE)
+
+## Set up database connection from compressed file
+R.utils::bunzip2("taxa.sqlite.bz2", remove = FALSE)
 
