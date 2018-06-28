a3406d0134c115cdb29b3ba6a1f3624cf8a3574e
Carl Boettiger <cboettig@gmail.com>
2018-06-28 16:37:50 -0700

    a few basic functions

---
+++ R/taxald.R
@@ -0,0 +1,90 @@
+
+## Look up id given a species name
+## Look up a heirarchy given a species name or species id
+## Return all species names / species ids belonging to a higher level rank
+## 
+## Look up a scientific name at any rank level.
+## 
+## Look up ids for higher ranks (when available)
+## Look up synonyms
+## Crosswalk / compare taxonomy across authorities in common format
+
+
+## Consider memoizing
+
+#' @importFrom dplyr right_join tibble collect
+#' 
+hierarchy <- function(species = NULL, 
+                      id = NULL, 
+                      authority = c("itis", "ncbi", "col", "tpl",
+                                    "gbif", "fb", "slb", "wd")){
+  
+  if(is.null(id) & !is.null(species)){
+    df <- dplyr::tibble(species)
+  } else if(!is.null(id) & is.null(species)){
+    df <- dplyr::tibble(id)
+  } else  if(!is.null(id) & !is.null(species)){
+    df <- dplyr::tibble(id, species)
+  } else {
+    stop("id or species list must be provided")
+  }
+  
+  
+  out <- dplyr::right_join(taxa_tbl(authority = authority,
+                                    schema = "hierarchy"), 
+                           df, copy = TRUE)
+  
+  dplyr::collect(out)
+  
+}
+
+
+ids <- function(name = NULL,
+                authority = c("itis", "ncbi", "col", "tpl",
+                              "gbif", "fb", "slb", "wd"),
+                schema = "taxonid"){
+
+  
+  out <- dplyr::right_join(
+    taxa_tbl(authority = authority, 
+             schema = "taxonid"), 
+             dplyr::tibble(name),
+             copy = TRUE) 
+    dplyr::collect(out)
+}
+
+descendents <- function(name = NULL, 
+                        rank = NULL, 
+                        id = NULL,
+                        authority = c("itis", "ncbi", "col", "tpl",
+                                      "gbif", "fb", "slb", "wd")){
+  authority <- match.arg(authority)
+  
+  ## if we have rank and name, filter on the hierarchy table
+  
+}
+
+
+#' @importFrom DBI dbConnect
+#' @importFrom MonetDBLite MonetDBLite
+#' @importFrom dbplyr src_dbi
+connect_db <- function(dbdir = Sys.getenv("TAXALD_HOME", 
+                                          fs::path(fs::path_home(),
+                                                   ".taxald"))){
+  con <- DBI::dbConnect(MonetDBLite::MonetDBLite(), dbdir)
+  db <- dbplyr::src_dbi(con)
+}
+
+#' @importFrom dplyr tbl
+taxa_tbl <- function(
+  db = connect_db(),
+  authority = c("itis", "ncbi", "col", "tpl",
+                "gbif", "fb", "slb", "wd"), 
+  schema = c("hierarchy", "taxonid", "synonyms")){
+  
+  authority <- match.arg(authority)
+  schema <- match.arg(schema)
+  tbl_name <- paste(authority, schema, sep = "_")
+  
+  dplyr::tbl(db, tbl_name)
+}
--- notebook/taxa-db.R
+++ notebook/taxa-db.R
@@ -1,87 +1,18 @@
 
 library(geiger)
 data(primates)
-df <- data_frame(species = gsub("_", " ", primates$phy$tip.label))
+species <- gsub("_", " ", primates$phy$tip.label)
 
-db <- connect_db()
 
 library(tictoc)
 tic()
 out <- right_join(tbl(con, "col_hierarchy"), df, copy = TRUE) %>% collect()
 toc()
-
-## Look up id given a species name
-## Look up a heirarchy given a species name or species id
-## Return all species names / species ids belonging to a higher level rank
-## 
-## Look up a scientific name at any rank level.
-## 
-## Look up ids for higher ranks (when available)
-## Look up synonyms
-## Crosswalk / compare taxonomy across authorities in common format
-
-
-## Consider memoizing
-hierarchy <- function(species = NULL, 
-                      id = NULL, 
-                      authority = c("itis", "ncbi", "col", "tpl",
-                                    "gbif", "fb", "slb", "wd")){
-  
-  right_join(taxa_tbl(authority, "hierarchy"), dplyr::tibble(species, id))
-}
-
-
-ids <- function(species = NULL, 
-                name = NULL,
-                authority = c("itis", "ncbi", "col", "tpl",
-                              "gbif", "fb", "slb", "wd")){
-  right_join(taxa_tbl(authority, "taxonid"), 
-             dplyr::tibble(name))
-}
-
-descendents <- function(name = NULL, 
-                        rank = NULL, 
-                        id = NULL,
-                        authority = c("itis", "ncbi", "col", "tpl",
-                                      "gbif", "fb", "slb", "wd")){
-  ## if we have rank and name, filter on the hierarchy table
-  
-}
-
-
-
-connect_db <- function(dbdir = Sys.getenv("TAXALD_HOME", 
-                                          fs::path(fs::path_home(),
-                                                   ".taxald"))){
-  con <- dbConnect(MonetDBLite::MonetDBLite(), dbdir)
-  db <- dplyr::src_dbi(con)
-}
-    
-taxald_table <- function(
-  db,
-  authority = c("itis", "ncbi", "col", "tpl",
-                "gbif", "fb", "slb", "wd"), 
-  schema = c("hierarchy", "taxonid", "synonyms")){
-  
-  authority <- match.arg(authority)
-  schema <- match.arg(schema)
-  tbl_name <- paste(authority, schema, sep = "_")
-  
-  dplyr::tbl(db, tbl_name)
-}
   
 #sqlite <- src_sqlite("taxa.sqlite")
 #tic()
 #out <- right_join(tbl(sqlite, "col_wide"), df, copy = TRUE) %>% collect()
 #toc()
-
-
-out <- right_join(tbl(con, "ncbi_taxonid"), df, copy = TRUE) %>% collect() %>% arrange(name)
-
-# out <- right_join(tbl(con, "itis_taxonid"), df, copy = TRUE) %>% collect() %>% arrange(name)
-
-
-out <- right_join(tbl(con, "ncbi_long"), df, copy = TRUE) %>% collect() %>% arrange(name)
 
 
 ## Support queries to preferred authority or multiple/all authorities 
