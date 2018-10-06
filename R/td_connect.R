0308aa778c7d62af0a9b93b61f435e81250fdd4b
Carl Boettiger <cboettig@gmail.com>
2018-10-05 19:40:10 -0700

    Cache established connection
    to avoid repeatedly calling connect. Also registers a finalizer to disconnect on exit from R.

--- R/td_connect.R
+++ R/td_connect.R
@@ -15,7 +15,27 @@
 #' 
 #' }
 td_connect <- function(dbdir = rappdirs::user_data_dir("taxald")){
+  
+  db <- mget("td_db", envir=taxald_cache, ifnotfound = NA)[[1]]
+  if(inherits(db, "DBIConnection")){
+    if(DBI::dbIsValid(db)){
+      return(db)
+    }
+  }
+    
   dbname <- file.path(dbdir, "monetdblite")
   dir.create(dbname, FALSE)
-  DBI::dbConnect(MonetDBLite::MonetDBLite(), dbname)
+  db <- DBI::dbConnect(MonetDBLite::MonetDBLite(), dbname)
+  assign("td_db", db, envir=taxald_cache)
+  
+  db
 }
+
+td_disconnect <- function(env=taxald_cache){
+  db <- mget("td_db", envir=env, ifnotfound = NA)[[1]]
+  if(inherits(db, "DBIConnection"))
+    DBI::dbDisconnect(db)
+}
+
+taxald_cache <- new.env()
+reg.finalizer(taxald_cache, td_disconnect, onexit = TRUE)
