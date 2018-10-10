52bba151e95ac7efd76ef30790fc057ff0d6fda0
Carl Boettiger <cboettig@gmail.com>
2018-10-10 14:25:00 -0700

    WIP

--- tests/testthat/test-all-dbs.R
+++ tests/testthat/test-all-dbs.R
@@ -6,34 +6,35 @@ library(dplyr)
 
 
 test_that("we can set up a db and call basic functions", {
-  
+
   skip_if(TRUE)
   skip_on_travis()
   skip_on_cran()
-  
+
   schema <- c("hierarchy", "taxonid", "synonyms")
+  schema2 <- c("hierarchy", "taxonid")
   overwrite = TRUE
-  td_create(authorities = "itis", schema = schema, overwrite = overwrite)
-  td_create(authorities = "ncbi", schema = schema, overwrite = overwrite)
-  td_create(authorities = "col", schema = schema)
-  td_create(authorities = "tpl")
-  td_create(authorities = "gbif", schema = c("hierarchy"))
-  td_create(authorities = "fb")
+  td_create(authorities = "itis")
+  td_create(authorities = "ncbi")
+  td_create(authorities = "col")
+  td_create(authorities = "gbif")
+  td_create(authorities = "fb",)
   td_create(authorities = "slb")
   td_create(authorities = "wd")
-  
-  
+  td_create(authorities = "tpl")
+
+
   td_create(authorities = "all")
 
-  
-  
+
+
   df <- taxa_tbl(authority = "col", schema = "hierarchy")
   chameleons <- df %>% filter(family == "Chamaeleonidae") %>% collect()
-  
+
   df <- descendants(name = "Aves", rank = "class")
   species <- ids(df$species)
   hier <- classification(df$species)
-  
+
   expect_is(df, "data.frame")
   expect_is(species, "data.frame")
   expect_is(hier, "data.frame")
--- tests/testthat/test-taxald.R
+++ tests/testthat/test-taxald.R
@@ -6,35 +6,35 @@ library(dplyr)
 
 
 test_that("we can set up a db and call basic functions", {
-  
+
+  taxald:::td_disconnect()
+
   tmp <- file.path(tempdir(), "taxald")
   dir.create(tmp, showWarnings = FALSE)
-  
+
   td_create(dbdir = tmp)
   db <- td_connect(tmp)
 
-  df <- taxa_tbl(authority = "itis", 
-                 schema = "hierarchy", 
+  df <- taxa_tbl(authority = "itis",
                  db = db)
-  
-  chameleons <- df %>% 
-    filter(family == "Chamaeleonidae") %>% 
+
+  chameleons <- df %>%
+    filter(family == "Chamaeleonidae") %>%
     collect()
-  
-  df <- descendants(name = "Aves", 
-                    rank = "class", 
+
+  df <- descendants(name = "Aves",
+                    rank = "class",
                     db = db)
-  species <- ids(df$species, 
+  species <- ids(df$species,
                  db = td_connect(tmp))
-  hier <- classification(df$species, 
+  hier <- classification(df$species,
                          db = db)
-  
+
   expect_is(df, "data.frame")
-  expect_is(species, "data.frame")
+  expect_is(species, "character")
   expect_is(hier, "data.frame")
   expect_is(chameleons, "data.frame")
   expect_gt(dim(df)[1], 1)
-  expect_gt(dim(species)[1], 1)
   expect_gt(dim(hier)[1], 1)
   expect_gt(dim(chameleons)[1], 1)
 
