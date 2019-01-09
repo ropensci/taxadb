f95cfcce610433aa86498faf87a17c38e356e571
Carl Boettiger <cboettig@gmail.com>
2019-01-08 19:08:51 -0500

    merge by synonyms

--- data-raw/join-all-providers.R
+++ data-raw/join-all-providers.R
@@ -1,20 +1,14 @@
 # Queries based on fully joined records
 library(dplyr)
+library(tidyr)
 library(taxadb)
 
-
-all_ids <- function(name = NULL,
-                collect = TRUE,
-                db = td_connect()){
-  sort <- TRUE # dummy name
-  input_table <- dplyr::tibble(name, sort = 1:length(name))
-
-  full <-
+full <-
   dplyr::full_join(select(taxa_tbl("itis", "taxonid"),
                           "itis" = "accepted_id", "name", "rank"),
                    select(taxa_tbl("ncbi", "taxonid"),
                           "ncbi" = "accepted_id", "name", "rank"),
-                  by = c("name", "rank")) %>%
+                   by = c("name", "rank")) %>%
   dplyr::full_join(select(taxa_tbl("ott", "taxonid"),
                           "ott" = "accepted_id", "name", "rank"),
                    by = c("name", "rank")) %>%
@@ -40,7 +34,42 @@ all_ids <- function(name = NULL,
                           "tpl" = "id", "name", "rank"),
                    by = c("name", "rank"))
 
-  # paste("SELECT * INTO newtable FROM", show_query(full))
+taxadb <- collect(select(full, name, rank, itis, ncbi, ott,
+                         iucn, wd, gbif, col, fb, slb, tpl))
+dir.create("data", FALSE)
+write_tsv(taxadb, "data/all_taxonid.tsv.bz2")
+#paste("SELECT * INTO all FROM", show_query(full))
+
+
+
+## We can merge things that are recognized as synonyms
+na_omit <- function(x) x[!is.na(x)]
+merge_rows <- function(df){
+  map_dfr(df, function(x){
+    compact <- na_omit(unique(x))
+    if(length(compact) == 0) return(as.character(NA))
+    if(length(compact) > 1) return(paste(compact, collapse = " | "))
+    compact
+  })
+}
+
+## Definitely includes things which are not synonyms
+merge_synonyms <-
+taxadb %>%
+  select(-name, -rank) %>%
+  group_by(iucn) %>%
+  nest() %>%
+  transpose() %>%
+  map_dfr(function(x) merge_rows(x$data))
+
+
+
+all_ids <- function(name = NULL,
+                collect = TRUE,
+                db = td_connect()){
+  sort <- TRUE # dummy name
+  input_table <- dplyr::tibble(name, sort = 1:length(name))
+
 
   ## replace synonym with accepted name in each case first?
 
