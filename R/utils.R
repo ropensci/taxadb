3f0bff6a004c6e68d5f4cab09929cb6bdcb6bdf1
Carl Boettiger <cboettig@gmail.com>
2019-02-06 15:57:50 -0800

    suppress unnecessary messages about SQL quoting

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 
-suppress_msg <- function(expr, pattern = "contain uppercase or reserved SQL characters"){
+suppress_msg <- function(expr, pattern = "reserved SQL"){
 withCallingHandlers(expr,
                     message = function(e){
                       if(grepl(pattern, e$message))
