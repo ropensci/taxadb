259aceb2671f6926dcdde189edcb03fc1e2832e7
Carl Boettiger <cboettig@gmail.com>
2019-03-03 22:19:10 -0800

    fix note

--- R/utils.R
+++ R/utils.R
@@ -7,27 +7,29 @@ withCallingHandlers(expr,
                     })
 }
 
-#' @importFrom dplyr count filter select right_join anti_join
+#' @importFrom dplyr count filter select right_join anti_join arrange
 #' @importFrom stats na.omit
 duplicate_as_unresolved <- function(df){
 
   ## Requires a column called sort that has 1 id for each input.
 
+  # NSE
   sort <- "sort"
+  n <- "n"
+
   multi_match <- df %>%
     dplyr::count(sort, sort = TRUE) %>%
     dplyr::filter(n > 1) %>%
     dplyr::select("sort")
 
-  input_id <- df %>% select("sort") %>% distinct()
+  input_id <- df %>% dplyr::select("sort") %>% dplyr::distinct()
 
   ##alternately, resolve multi-match with top_n?
 
   ## Drop multi-match
-  anti_join(df, multi_match, by = "sort") %>%
+  dplyr::anti_join(df, multi_match, by = "sort") %>%
   ## And replace as NA
-    right_join(input_id, by = "sort") %>%
-    arrange(sort)
-
+    dplyr::right_join(input_id, by = "sort") %>%
+    dplyr::arrange(sort)
 
 }
