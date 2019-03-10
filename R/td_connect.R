c25f3d72110af756952d394e1210226ce623c9b1
Carl Boettiger <cboettig@gmail.com>
2019-03-10 13:32:00 -0700

    update docs

--- R/td_connect.R
+++ R/td_connect.R
@@ -1,26 +1,48 @@
 #' Connect to the taxadb database
 #'
 #' @param dbdir Path to the database.
-#' @return Returns a `src_dbi` connection to the database
-#' @details Primarily useful when a lower-level interface to the
-#' database is required.  Most `taxadb` functions will connect
-#' automatically without the user needing to call this function.
-#' @importFrom DBI dbConnect
+#' @return Returns a `src_dbi` connection to the default MonetDBLite database
+#' @details This function provides a MonetDBLite database connection for
+#' `taxadb`. Note that `taxadb` functions provide drop-in support for most
+#' relational database connections as an alternative to the MonetDBLite
+#' option -- simply supply a different `src_dbi` connection in place of
+#' the one returned by `td_connect()`.
+#'
+#' The MonetDBLite connection provided by this function is set as the default
+#' option because it can be automatically installed as an embedded database
+#' from R, much like SQLite, without requiring a seperate server instance.
+#' MonetDBLite is much faster (particularly for joins) and more feature-rich
+#' than SQLite (e.g. supporting windowing functions). One drawback of the
+#' embeded database is the inability to support concurrent connections from
+#' multiple R sessions.  Either limit access to the local database to a
+#' single R session at a time, or provide an alternative TAXADB_HOME path
+#' to start a second session with a separate database.  Alternatively,
+#' some users may prefer a free-standing server connection to support concurrent
+#' connections.  We recommend MonetDB (server version) for this for the
+#' best performance. `taxadb` will work with other database connections such
+#' as Postgres or MariaDB, but queries (filtering joins) will be much slower
+#' on these non-columnar databases.
+#'
+#' For performance reasons, this function will also cache and restore the
+#' existing database connection, making repeated calls to `td_connect()` much
+#' faster and more failsafe than repeated calls to [DBI::dbConnect]
+#'
+#'
+#' @importFrom DBI dbConnect dbIsValid
 #' @importFrom MonetDBLite MonetDBLite
 #' @export
-#' @examples \dontrun{
+#' @examples \donttest{
+#' ## OPTIONAL: you can first set an alternative home location,
+#' ## such as a temporary directory:
+#' Sys.setenv(TAXADB_HOME=tempdir())
 #'
+#' ## Connect to the database:
 #' db <- connect_db()
 #'
 #' }
 td_connect <- function(dbdir = taxadb_dir()){
 
   dbname <- file.path(dbdir, "monetdblite")
-
-  ## Stop if monetdb is locked (monetdblite/.gdk_lock)
-  ## CANNOT Just check if this file exists -- that breaks every connection!
-  ## Stop if monetdb is pointing to another location from the same session
-
   db <- mget("td_db", envir = taxadb_cache, ifnotfound = NA)[[1]]
   if (inherits(db, "DBIConnection")) {
     if (DBI::dbIsValid(db)) {
@@ -29,9 +51,22 @@ td_connect <- function(dbdir = taxadb_dir()){
   }
 
   dir.create(dbname, FALSE)
-  db <- DBI::dbConnect(MonetDBLite::MonetDBLite(), dbname)
-  assign("td_db", db, envir = taxadb_cache)
 
+  tryCatch(
+    db <- DBI::dbConnect(MonetDBLite::MonetDBLite(), dbname),
+    error = function(e){
+      if(grepl("Database lock", e))
+        stop(paste("Local taxadb database is locked by another R session.\n",
+                 "Try closing that session first or set the TAXADB_HOME\n",
+                  "environmental variable to a new location.\n"),
+             call. = FALSE)
+      else stop(e)
+    },
+    finally = return(NULL)
+  )
+
+
+  assign("td_db", db, envir = taxadb_cache)
   db
 }
 
@@ -44,6 +79,8 @@ td_disconnect <- function(env = taxadb_cache){
   }
 }
 
+## Enironment to store the cached copy of the connection
+## and a finalizer to close the connection on exit.
 taxadb_cache <- new.env()
 reg.finalizer(taxadb_cache, td_disconnect, onexit = TRUE)
 
